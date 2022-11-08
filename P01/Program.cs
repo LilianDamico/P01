88f356e422366bfe4f717e90b9030853dbcc15d6
Fatec <Fatec@DESKTOP-78MS2FN>
2022-11-08 17:02:06 -0200

    Adicionar arquivos de projeto.

---
+++ P01/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.Serialization;
+int soma = 0;
+int media, nota = 0;
+int i;
+int t;
+Console.WriteLine("Informe quantas notas deseja inserir: ");
+t = Convert.ToInt32(Console.ReadLine());
+do
+{
+    for (i = 0; i < t; i++)
+    {
+        Console.Write("Informe a nota: ");
+        nota = Convert.ToInt32(Console.ReadLine());
+        soma = soma + nota;
+    }
+    media = soma / t;
+} while (nota < 0 || nota > 10);
+Console.WriteLine("A média final foi: " + media);
+Console.ReadKey();
